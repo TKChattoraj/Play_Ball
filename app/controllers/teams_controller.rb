40730fe2d7e37c50f37a7f113396a915f17b03ae
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2016-04-09 16:25:07 -0400

    Create Session, revise application.html and team#show view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,7 @@
 class TeamsController < ApplicationController
+
+def show
+  @team = Team.find(params[:id])
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :teams
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -59,4 +61,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'welcome#index'
 end
