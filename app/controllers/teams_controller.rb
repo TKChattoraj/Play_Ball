d525b76e412e185d0477044c4e5c97e628edb575
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-02-27 23:37:13 -0500

    adding players to the team show method and providing player list with hitting stats in the team show view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -3,7 +3,8 @@ class TeamsController < ApplicationController
 def show
   @team = Team.find(params[:id])
   @notes = @team.notes.recent
-  @players = Player.get_team
+  @players = Player.on_team(@team)
+
 end
 
 end
