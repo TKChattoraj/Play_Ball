0d3765a232e31372dba20ce9c1ec52a84cea4494
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-03-08 23:21:39 -0500

    defined the games create action, began the form view for a new game allowing choosing visitors and home teams

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,23 @@ class GamesController < ApplicationController
 
   end
 
+  def create
+    @game = Game.new(game_params)
+    # puts @game.location
 
 
+    if @game.save
+      flash[:notice] = "New Game Scheduled!"
+      redirect_to games_path
+    else
+      flash[:notice] = "Could Not Schedule a Game.  Try Again."
+      redirect_to new_games_path
+    end
+  end
+
+private
+  def game_params
+    params.require(:game).permit(:home_id, :visitors_id, :location_id, :date, :time)
+  end
 
 end
