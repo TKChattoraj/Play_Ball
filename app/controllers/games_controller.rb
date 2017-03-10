f8631250bd0e2dea3b2b5998811a640de50fa26c
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-03-10 15:14:11 -0500

    created games#decide_game_view route and action, created framework for game#edit view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,36 @@
 class GamesController < ApplicationController
 
+  def index
+    @games = Game.all
+  end
+
+
   def new
     @game = Game.new
 
   end
+
+  def show
+    @game = Game.find(game_id)
+  end
+
+  def decide_game_view
+    @game = Game.find(game_id)
+
+
+
+    if @game.winner && @game.loser
+      puts 'winner/loser'
+      redirect_to @game
+    else
+      redirect_to edit_game_path(@game)
+    end
+  end
+
+  def edit
+
+  end
+
 
   def create
     @game = Game.new(game_params)
@@ -22,6 +49,9 @@ class GamesController < ApplicationController
 private
   def game_params
     params.require(:game).permit(:home_id, :visitors_id, :location_id, :date, :time)
+  end
+  def game_id
+    params.require(:id)
   end
 
 end
