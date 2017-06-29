8f6be1ae9b0dd10480686c83e55d89941a4da435
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-06-28 23:14:59 -0400

    providing player pitching stats view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -50,11 +50,13 @@ def show
   @player = Player.find(params[:id])
 
   @player_hitting_total = HittingTotal.find_by(player_id: @player.id)
-  @player_pitching_total = PitchingTotal.find_by(player_id: @player.id)
-
+  @player_pitching_total = @player.pitching_total
 
   @player_hitting_stats = @player.game_hitting_stats
-  @player_pitching_stats = @player.game_pitching_stats
+
+  if @player.pitcher?
+    @player_pitching_stats = @player.game_pitching_stats
+  end
 
 end
 
