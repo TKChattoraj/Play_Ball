280acc2c43e8849c7f0625c00a963ad4d4bd5d73
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-08-27 13:33:30 -0400

    allowing any number to successfully text an alert

--- app/controllers/api/texts_controller.rb
+++ app/controllers/api/texts_controller.rb
@@ -27,21 +27,13 @@ before_action :set_access_control_headers
 
   def create
 
-    #raise params[:From].inspect
-
     @cell = params[:From].slice!(-10, 10)
-    puts @cell
-    # begin
 
-    @user = User.find_by(cell: @cell)
-    # if @user.nil?
-    #   render plain: 'You are not authoried to post a note at cabl.'
-    #   return
-      # end
-    # rescue
-    #   # ...respond with unauthorized
-    # end
-    puts @user.last_name
+    # For real, we want the method to determine whether the phone texitng the message is from an authorized user--manager.  For demonstration, however, the @user is going to be designated as Sparky
+
+    #@user = User.find_by(cell: @cell)
+    @user = User.find_by(first_name: 'Sparky')
+
 
 
     #if @user and @user.manager # manager method defined in User model
@@ -52,12 +44,7 @@ before_action :set_access_control_headers
       @active_alerts = Alert.active
 
       if @alert.save
-        # alerts = ""
-        # @active_alerts.each do |a|
-        #   alerts = alerts + "From: " + a.user.first_name + "-" + a.content + "\n"
-        # end
-        # flash[:alert] = alerts
-        # puts flash[:alert]
+
         ActionCable.server.broadcast 'text_channel', name: @alert.user.first_name, content: @alert.content
         render template: "api/texts/post_success.xml.erb", layout: false, content_type: "application/xml"
         return
