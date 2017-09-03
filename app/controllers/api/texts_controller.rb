81cde74755472bc2b2db46825eac8210849fea12
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-09-03 16:28:30 -0400

    chaning to attribute alert to the proper user

--- app/controllers/api/texts_controller.rb
+++ app/controllers/api/texts_controller.rb
@@ -33,15 +33,16 @@ before_action :set_access_control_headers
     # For real, we want the method to determine whether the phone texitng the message is from an authorized user--manager.  For demonstration, however, the @user is going to be designated as Sparky
 
     #@user = User.find_by(cell: @cell)
-    @user = User.find_by(first_name: 'Sparky')
+    #@user = User.find_by(first_name: 'Sparky')
 
 
 
     #if @user and @user.manager # manager method defined in User model
-    if @user
+    if current_user
+      @user = current_user
       @alert = Alert.new
       @alert.content = params[:Body]
-      @alert.user_id = @user.id
+      @alert.user_id = current_user.id
       @active_alerts = Alert.active
 
       if @alert.save
