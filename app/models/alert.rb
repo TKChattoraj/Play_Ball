4366533c14bf4221cd20b53b340a17a9f43bc13b
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-09-03 16:17:35 -0400

    changing the time_ago for alerts

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,12 +2,12 @@ class Alert < ApplicationRecord
   belongs_to :user
 
   def self.active
-    # days_ago = 2
-    # time_ago = days_ago.days.ago.getlocal
+    days_ago = 2
+    time_ago = days_ago.days.ago.getlocal
 
-    minutes_ago = 10
-
-    time_ago = minutes_ago.minutes.ago.getlocal
+    # minutes_ago = 10
+    #
+    # time_ago = minutes_ago.minutes.ago.getlocal
     where("created_at > ?", time_ago)
   end
 
