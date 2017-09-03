d1e7a692845dfe51f5d049a2c68795cf7b75166f
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2017-09-03 11:09:19 -0400

    slight edit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
 
     #welcome#index view calls standings partial--which contains the index functionality embedded at the begining.  This is done because the standings partial is used throughout the site.
-    flash[:notice] = "Welcome.  This is a demo site. Anyone can use with the pre-built mock registration.  Just hit 'Sign In' and follow the prompts.  Your personal stuff is not needed (or wanted)."
+    flash[:notice] = "Welcome.  This is a demo site. Anyone can use it with the pre-built mock registration.  Just hit 'Sign In' and follow the prompts.  Your personal stuff is not needed (or wanted)."
   end
 
   def about
